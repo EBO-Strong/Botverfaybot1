82e50bb255ab45b4d76f1f73c69b6d8caca2e744
EBO-Strong <93388312+EBO-Strong@users.noreply.github.com>
2021-11-07 22:11:26 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-    token:"NzYwODQxMDI0MDE5ODkwMjU3.X3R6aw.s6zMmiFYTWAMVcmemAuZNEnxgZs",
+    token:"NzYwODQxMDI0MDE5ODkwMjU3.X3R6aw.odIEY7tM-CX1Ig4b_QKUrE6_XYU",
     mongoPath:"mongodb+srv://ALEX:hvgqXetXFfBbVzb6@cluster0.epc7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     prefix: "a!",
     expressURL: "" // if you have a express server but it here to uptime you project
